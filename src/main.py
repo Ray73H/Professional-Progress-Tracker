81fa551b16a8323744aeffdd0ce5ccb7bd1a5f31
Mervyn <mervynzwkoh@gmail.com>
2025-06-01 18:56:33 +0200

    created dashboard page

--- src/main.py
+++ src/main.py
@@ -3,9 +3,4 @@ from src import create_app
 app = create_app()
 
 if __name__ == '__main__':
-<<<<<<< HEAD:main.py
-    app.run(debug=True)
-    
-=======
     app.run(host="0.0.0.0", port=5000)
->>>>>>> 138cb5d55d631323e71079a703c623d3b4d484a2:src/main.py
